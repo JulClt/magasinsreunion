f7b0c14c4ff8a5e981a4ad1f4a0237951a9e9e4a
Julien <julien.collet01@gmail.com>
2014-02-10 22:04:01 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Magasinsreunion::Application.config.secret_key_base = 'd3646e91dcb6f65308aa704796e0ba067a67d0e8b94b8eafdb6fd3fca51050a6d693d9b111355099db991445f0e6ef480c320d557fd500bdad392b06856632de'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Magasinsreunion::Application.config.session_store :cookie_store, key: '_magasinsreunion_session'
