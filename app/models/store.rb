8f4829676f401de5e08c407b5c66c35e7f177cb3
Julien <julien.collet01@gmail.com>
2014-03-15 14:20:45 +0100

    Modify has_attached_fil in store object

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,3 +1,3 @@
 class Store < ActiveRecord::Base
-	has_attached_file :logo, :style => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "http://placehold.it/100x100" 
+	has_attached_file :logo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "http://placehold.it/100x100" 
 end
